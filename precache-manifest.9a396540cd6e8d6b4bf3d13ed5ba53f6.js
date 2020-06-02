e741f91c4438ce457f61f7f4604226d11dbf8c27
Chayan9231 <duttachayan600@gmail.com>
2020-06-02 17:37:29 +0530

    Updates

---
+++ precache-manifest.9a396540cd6e8d6b4bf3d13ed5ba53f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2162acaf40c46c678552f1d26f396ffe",
+    "url": "/BookReactApplication/index.html"
+  },
+  {
+    "revision": "ad4b5193bdd3a2e9b106",
+    "url": "/BookReactApplication/static/css/main.aaf689f3.chunk.css"
+  },
+  {
+    "revision": "1fb775a6bfe8b2e3ca97",
+    "url": "/BookReactApplication/static/js/2.6190dd27.chunk.js"
+  },
+  {
+    "revision": "0042ed35eedf85f73c564b78f2a06746",
+    "url": "/BookReactApplication/static/js/2.6190dd27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad4b5193bdd3a2e9b106",
+    "url": "/BookReactApplication/static/js/main.f22d7f89.chunk.js"
+  },
+  {
+    "revision": "0aa2b5fbeb9aeecf829d",
+    "url": "/BookReactApplication/static/js/runtime-main.7397783b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BookReactApplication/static/media/logo.5d5d9eef.svg"
+  }
+]);
